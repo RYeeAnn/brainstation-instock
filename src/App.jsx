bff2ca3957c294ba3d948337d2bbbb819b305a75
Ryan <ryeean16@gmail.com>
2023-08-15 02:08:49 -0700

    instock react start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Router>
+      <Router>
+        
+      </Router>
+    </Router>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
