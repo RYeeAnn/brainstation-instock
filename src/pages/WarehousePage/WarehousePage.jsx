741a25639bea04fd173f97d96bdb8e3f6c9d7a02
supreetkaur <supreet97@gmail.com>
2023-08-16 14:53:53 -0700

    deleted duplicate warehousePage.jsx

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,9 +1,12 @@
+import React from "react";
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
 import WarehouseDataList from "../../components/WarehouseDataList/WarehouseDataList";
 
 function WarehousePage() {
     return (
         <>
-            <WarehouseDataList />
+        <WarehouseDetails />
+        <WarehouseDataList />
         </>
     );
 }
