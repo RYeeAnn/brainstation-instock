d9a13359684428135e71d3eb3ae2d0c18fbce1f4
supreetkaur <supreet97@gmail.com>
2023-08-16 16:41:43 -0700

    some styling for warehouse data list

--- src/components/WarehouseDataList/WarehouseDataList.jsx
+++ src/components/WarehouseDataList/WarehouseDataList.jsx
@@ -1,8 +1,11 @@
 import "./WarehouseDataList.scss"
 import DeleteIcon from "../../assets/Icons/delete_outline-24px.svg"
 import EditIcon from "../../assets/Icons/edit-24px.svg"
+import SortIcon from "../../assets/Icons/sort-24px.svg"
+import ChevronIcon from "../../assets/Icons/chevron_right-24px.svg"
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom"
 // import { useParams } from "react-router-dom";
 
 function WarehouseDataList() {
@@ -21,40 +24,55 @@ function WarehouseDataList() {
     )
 
     return (
+        <section className="warehouse-data-list__section">
         <ul className="warehouse-data-list__header-unordered-list">
             <li className="warehouse-data-list__header-list">
-                <p className="warehouse-data-list__header-list-items">WAREHOUSE</p>
-                <p className="warehouse-data-list__header-list-items">ADDRESS</p>
-                <p className="warehouse-data-list__header-list-items">CONTACT NAME</p>
-                <p className="warehouse-data-list__header-list-items">CONTACT INFORMATION</p>
-                <p className="warehouse-data-list__header-list-items">ACTIONS</p>
+                <div className="warehouse-data-list__header">
+                    <p className="warehouse-data-list__header-list-items">WAREHOUSE</p>
+                    <img className="warehouse-data-list__header-sort" src={SortIcon} alt="sort icon"/>
+                </div>
+                <div className="warehouse-data-list__header">
+                    <p className="warehouse-data-list__header-list-items">ADDRESS</p>
+                    <img className="warehouse-data-list__header-sort" src={SortIcon} alt="sort icon"/>
+                </div>
+                <div className="warehouse-data-list__header">
+                    <p className="warehouse-data-list__header-list-items">CONTACT NAME</p>
+                    <img className="warehouse-data-list__header-sort" src={SortIcon} alt="sort icon"/>
+                </div>
+                <div className="warehouse-data-list__header">
+                    <p className="warehouse-data-list__header-list-items">CONTACT INFORMATION</p>
+                    <img className="warehouse-data-list__header-sort" src={SortIcon} alt="sort icon"/>
+                </div>
+                <div className="warehouse-data-list__header">
+                    <p className="warehouse-data-list__header-list-items">ACTIONS</p>
+                </div>
             </li>
 
             {
                 warehouseData.map((warehouse) => {
                     return (
-                        <li key={warehouse.id} className="warehouse-data-list__header-list">
-                            <p>{warehouse.warehouse_name}</p>
-                            <p>{warehouse.address}</p>
-                            <p>{warehouse.contact_name}</p>
-                            <p>
-                                <div>
+                        <li key={warehouse.id} className="warehouse-data-list__content-list">
+                            <div className="warehouse-data-list__content-list-items">
+                                <NavLink to={`/warehouses/${warehouse.id}`} className="warehouse-data-list__content-warehouse">{warehouse.warehouse_name}</NavLink>
+                                <img className="warehouse-data-list__content-warehouse-chevron" src={ChevronIcon} alt="chevron icon" />
+                            </div>
+                            <p className="warehouse-data-list__content-list-items">{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
+                            <p className="warehouse-data-list__content-list-items">{warehouse.contact_name}</p>
+                            <div className="warehouse-data-list__content-list-items warehouse-data-list__content-list-items--column">
                                     <p>{warehouse.contact_phone}</p>
                                     <p>{warehouse.contact_email}</p>
-                                </div>
-                            </p>
-                            <p>
-                                <div>
-                                    <img src={DeleteIcon} />
-                                    <img src={EditIcon} />
-                                </div>
-                            </p>
+                            </div>
+                            <div className="warehouse-data-list__content-list-items">
+                                    <img className="warehouse-data-list__content-list-images warehouse-data-list__content-list-images--left" src={DeleteIcon} />
+                                    <img className="warehouse-data-list__content-list-images" src={EditIcon} />
+                            </div>
                         </li>
                     )
                 })
             }
 
         </ul>
+        </section>
     );
 }
 
