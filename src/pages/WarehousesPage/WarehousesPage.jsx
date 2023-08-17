e25615728d4f9f64f352b37e9f2e20580d8d53ad
supreetkaur <supreet97@gmail.com>
2023-08-16 22:55:29 -0700

    mobile styling - warehouse list

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
 import WarehouseDataList from "../../components/WarehouseDataList/WarehouseDataList";
 import TitleBar from "../../components/TitleBar/TitleBar";
+import "./WarehousesPage.scss";
 
 function WarehousesPage() {
     return (
-        <div>
+        <div className="warehouses-section">
         <TitleBar />
         <WarehouseDataList />
         </div>
