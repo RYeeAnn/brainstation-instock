bcf59a639cc677bd4dabea3b296f8406315d307b
Richard Ly <richardly9319@gmail.com>
2023-08-17 14:28:17 -0700

    fixed minor post merge bugs

--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
 import WarehouseDataList from "../../components/WarehouseDataList/WarehouseDataList";
 import WarehouseTitleBar from "../../components/WarehouseTitleBar/WarehouseTitleBar";
+import TitleBar from "../../components/TitleBar/TitleBar";
+
 import "./WarehousesPage.scss";
+
 
 function WarehousesPage() {
     return (
         <div className="warehouses-section">
-        <WarehouseTitleBar />
+        <TitleBar />
         <WarehouseDataList />
         </div>
     );
