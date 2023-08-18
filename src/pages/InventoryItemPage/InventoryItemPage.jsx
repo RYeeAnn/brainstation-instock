06be5226759c6b8ecd06325cd094fd210fd38945
Ryan <ryeean16@gmail.com>
2023-08-18 15:58:42 -0700

    updated my auto fill again to edit forms after merge conflicts

--- src/pages/InventoryItemPage/InventoryItemPage.jsx
+++ src/pages/InventoryItemPage/InventoryItemPage.jsx
@@ -3,67 +3,54 @@ import './InventoryItemPage.scss';
 import { useParams } from 'react-router';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import InventoryEditForm from '../../components/InventoryEditForm/InventoryEditForm';
 
 function InventoryItemPage() {
-
     let api_url = process.env.REACT_APP_API_URL;
     let { itemID } = useParams();
 
-    const [itemName, setItemName] = useState("");
-    const [itemDescription, setItemDescription] = useState("");
-    const [itemCategory, setItemCategory] = useState("");
-    const [itemStatus, setItemStatus] = useState("");
-    const [itemQuantity, setItemQuantity] = useState(0);
-    const [Warehouse, setWarehouse] = useState("");
-
+    const [itemData, setItemData] = useState({});
 
     useEffect(() => {
         axios.get(`${api_url}/inventories/${itemID}`).then((res) => {
-            setItemName(res.data.item_name);
-            setItemDescription(res.data.description);
-            setItemCategory(res.data.category);
-            setItemStatus(res.data.status);
-            setItemQuantity(res.data.quantity);
-            setWarehouse(res.data.warehouse_name);
+            setItemData(res.data);
         });
-    }, []);
-    
+    }, [itemID]);
 
-  return (
-    <div className="inventory-Item-Page">
-        <PageTitleBar title={itemName} edit={true} />
-        <div className="inventory-Item__overall-container">
-
-        
-        <div className="inventory-Item__description-section1">
-        <div className="inventory-Item__description-field-container">
-        <h4>ITEM DESCRIPTION:</h4>
-        <p>{itemDescription}</p>
+    return (
+        <div className="inventory-Item-Page">
+            <PageTitleBar title={itemData.item_name} edit={true} />
+            <div className="inventory-Item__overall-container">
+                <div className="inventory-Item__description-section1">
+                    <div className="inventory-Item__description-field-container">
+                        <h4>ITEM DESCRIPTION:</h4>
+                        <p>{itemData.description}</p>
+                    </div>
+                    <div className="inventory-Item__description-field-container">
+                        <h4>CATEGORY:</h4>
+                        <p>{itemData.category}</p>
+                    </div>
+                </div>
+                <div className="inventory-Item__description-section2">
+                    <div className="inventory-Item__status-quantity-container">
+                        <div className="inventory-Item__description-field-container">
+                            <h4>STATUS:</h4>
+                            <p>{itemData.status}</p>
+                        </div>
+                        <div className="inventory-Item__description-field-container">
+                            <h4>QUANTITY:</h4>
+                            <p>{itemData.quantity}</p>
+                        </div>
+                    </div>
+                    <div className="inventory-Item__description-field-container">
+                        <h4>WAREHOUSE:</h4>
+                        <p>{itemData.warehouse_name}</p>
+                    </div>
+                </div>
+            </div>
+            <InventoryEditForm itemData={itemData} />
         </div>
-        <div className="inventory-Item__description-field-container">
-        <h4>CATEGORY:</h4>
-        <p>{itemCategory}</p>
-        </div>
-        </div>
-        <div className="inventory-Item__description-section2">
-        <div className="inventory-Item__status-quantity-container">
-        <div className="inventory-Item__description-field-container">
-        <h4>STATUS:</h4>
-        <p>{itemStatus}</p>
-        </div>
-        <div className="inventory-Item__description-field-container">
-        <h4>QUANTITY:</h4>
-        <p>{itemQuantity}</p>
-        </div>
-        </div>
-        <div className="inventory-Item__description-field-container">
-        <h4>WAREHOUSE:</h4>
-        <p>{Warehouse}</p>
-        </div>
-        </div>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default InventoryItemPage
+export default InventoryItemPage;
