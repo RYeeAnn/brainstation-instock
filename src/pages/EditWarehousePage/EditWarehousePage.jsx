3ee29cfd0e64c1300362794d5e214545b26058d5
supreetkaur <supreet97@gmail.com>
2023-08-18 14:44:59 -0700

    pre-filled fields for edit warehouse page

--- src/pages/EditWarehousePage/EditWarehousePage.jsx
+++ src/pages/EditWarehousePage/EditWarehousePage.jsx
@@ -3,13 +3,19 @@ import PageTitleBar from '../../components/PageTitleBar/PageTitleBar';
 import WarehouseDetailsForm from '../../components/WarehouseDetailsForm/WarehouseDetailsForm';
 import ButtonAddWarehouse from '../../components/ButtonAddWarehouse/ButtonAddWarehouse';
 import ButtonCancel from '../../components/ButtonCancel/ButtonCancel';
+import { useParams } from "react-router-dom";
 
 
-function EditWarehousePage(props) {
+function EditWarehousePage() {
+
+    const {warehouseID} = useParams();
+    // console.log(warehouseID)
+    // console.log(props);
+
     return (
         <div className="editWarehouse">
         <PageTitleBar title='Edit Warehouse' edit={false} />
-        <WarehouseDetailsForm calledFrom="EditWarehousePage" />
+        <WarehouseDetailsForm calledFrom="EditWarehousePage" warehouseID={warehouseID} />
     </div>
     );
 }
