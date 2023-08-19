e307e3aa511cedc9e749b9961cc4bd099d81e731
supreetkaur <supreet97@gmail.com>
2023-08-19 01:08:50 -0700

    bring in data from back-end for single warehouse page (with all its inventory details)

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,10 +1,44 @@
 import './WarehousePage.scss';
 import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function WarehousePage() {
+
+  const [inventoriesForWarehouse, setinventoriesForWarehouse] = useState([]);
+
+  const { warehouseID } = useParams();
+
+  const api_URL = `${process.env.REACT_APP_API_URL}`
+  axios.get(`${api_URL}/warehouses/${warehouseID}/inventories`)
+    .then((response) => {
+      console.log(response.data);
+      setinventoriesForWarehouse(response.data);
+    })
+    .catch((error) => {
+      console.timeLog(error)
+    });
+
+
   return (
-    <WarehouseDetails />
-    
+    <>
+      <WarehouseDetails />
+
+      <li>
+        {inventoriesForWarehouse.map((inventory) => {
+          return (
+            <>
+              <p>{inventory.item_name}</p>
+              <p>{inventory.category}</p>
+              <p>{inventory.status}</p>
+              <p>{inventory.quantity}</p>
+            </>
+          )
+        })}
+      </li>
+    </>
+
   )
 }
 
