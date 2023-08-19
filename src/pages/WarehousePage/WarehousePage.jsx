98eb32d31a359d393aa8fa56c16d5bf394455e35
Ryan <ryeean16@gmail.com>
2023-08-19 15:20:46 -0700

    updated version of warehouse and inventory page

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,5 +1,34 @@
 import './WarehousePage.scss';
 import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
+import PageTitleBar from '../../components/PageTitleBar/PageTitleBar';
+import InventoryDataList from '../../components/InventoryDataList/InventoryDataList';
+
+import { useParams } from 'react-router';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function WarehousePage() {
+
+  let api_url = process.env.REACT_APP_API_URL;
+  let { warehouseID } = useParams();
+
+
+  const [warehouseName, setWarehouseName] = useState("");
+
+  useEffect(() => {
+    axios.get(`${api_url}/warehouses/${warehouseID}`).then((res) => {
+        setWarehouseName(res.data[0].warehouse_name);
+
+    });
+}, []);
+
+  return (
+    <div>
+    <PageTitleBar title={warehouseName} edit={true} />
+    <WarehouseDetails />
+    <InventoryDataList warehouseID={warehouseID} />
+
+    </div>
 
   )
 }
