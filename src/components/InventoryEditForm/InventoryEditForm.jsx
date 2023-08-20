df56beec496f284fd590620bfea127d7898247ef
Ryan <ryeean16@gmail.com>
2023-08-20 16:55:40 -0700

    edit inventory when clicking instock or outofstock dynamic update

--- src/components/InventoryEditForm/InventoryEditForm.jsx
+++ src/components/InventoryEditForm/InventoryEditForm.jsx
@@ -13,64 +13,66 @@ function InventoryEditForm() {
   const [category, setCategory] = useState("");
   const [warehouse, setWarehouse] = useState("");
   const [warehouseList, setWarehouseList] = useState([]);
-  
+  const [quantity, setQuantity] = useState("");
+  const [quantityError, setQuantityError] = useState(""); // Added for quantity error
+
   const [itemNameError, setItemNameError] = useState("");
   const [descriptionError, setDescriptionError] = useState("");
   const [categoryError, setCategoryError] = useState("");
   const [warehouseError, setWarehouseError] = useState("");
 
   const categories = [
-    'Electronics', 'Gear','Apparel', 'Accessories','Health','Gear'
+    'Electronics', 'Gear', 'Apparel', 'Accessories', 'Health', 'Gear'
   ];
 
   let api_url = process.env.REACT_APP_API_URL;
-  let { itemID } = useParams();
 
+  let { itemID } = useParams();
   const [itemData, setItemData] = useState({});
 
   useEffect(() => {
-
-    // Fetch inventory data using inventoryID
     axios
       .get(`${process.env.REACT_APP_API_URL}/inventories/${params.itemID}`)
       .then((response) => {
         const inventoryData = response.data;
-        console.log('inventoryData', inventoryData)
         setItemName(inventoryData.item_name);
         setDescription(inventoryData.description);
         setCategory(inventoryData.category);
         setStatus(inventoryData.status);
         setWarehouse(inventoryData.warehouse_id);
-        
+        setQuantity(inventoryData.quantity);
       })
       .catch((error) => {
         console.error("Error fetching inventory details:", error);
       });
 
-
-      axios.get(`${process.env.REACT_APP_API_URL}/warehouses/`).then(response=>{
+    axios.get(`${process.env.REACT_APP_API_URL}/warehouses/`)
+      .then(response => {
         setWarehouseList(response.data);
-      }).catch(err => console.error(err))
-
+      })
+      .catch(err => console.error(err));
   }, [params]);
 
   useEffect(() => {
-    axios.get(`${api_url}/inventories/${itemID}`).then((res) => {
+    axios.get(`${api_url}/inventories/${itemID}`)
+      .then((res) => {
         setItemData(res.data);
-    });
-}, [itemID]);
+      });
+  }, [itemID]);
 
   const handleStatusChange = (event) => {
     setStatus(event.target.value);
+    if (event.target.value === "Out of Stock") {
+      setQuantity("");
+      setQuantityError(""); // Clear quantity error when status changes
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    
-    
     let valid = true;
-    
+
     if (itemName === "") {
       setItemNameError("Item Name is required.");
       valid = false;
@@ -92,31 +94,38 @@ function InventoryEditForm() {
       setCategoryError("");
     }
 
+    if (status !== "Out of Stock" && quantity === "") {
+      setQuantityError("Quantity is required.");
+      valid = false;
+    } else {
+      setQuantityError("");
+    }
+
     if (warehouse === "") {
       setWarehouseError("Warehouse is required.");
       valid = false;
     } else {
       setWarehouseError("");
     }
-    
-    if (valid) {
 
+    if (valid) {
       let updated_inventoryItem = {
         "warehouse_id": event.target.warehouse.value,
         "item_name": event.target.itemName.value,
         "description": event.target.description.value,
         "category": event.target.category.value,
         "status": event.target.status.value,
-    }
+        "quantity": event.target.quantity.value
+      };
 
-      console.log("UPDATED INVNETORY ITEM: ", updated_inventoryItem);
-
-      axios.put(`${process.env.REACT_APP_API_URL}/inventories/${params.itemID}`, updated_inventoryItem).then(
-        res => {
+      axios.put(`${process.env.REACT_APP_API_URL}/inventories/${params.itemID}`, updated_inventoryItem)
+        .then(res => {
           alert("Successfully edited Inventory Item!");
           navigate('../inventory');
-        }
-      )
+        })
+        .catch(error => {
+          console.error("Error editing inventory item:", error);
+        });
     }
   };
 
@@ -130,7 +139,6 @@ function InventoryEditForm() {
             <label>Item Name</label>
             <br />
             <input
-              placeholder="Item Name"
               className={`inventoryDetailsForm__itemName-input ${itemNameError && 'error'}`}
               type="text"
               name="itemName"
@@ -143,7 +151,6 @@ function InventoryEditForm() {
             <label>Description</label>
             <br />
             <textarea
-              placeholder="Please enter a brief item description..."
               className={`inventoryDetailsForm__description-input ${descriptionError && 'error'}`}
               type="text"
               name="description"
@@ -161,10 +168,9 @@ function InventoryEditForm() {
               value={category}
               onChange={(e) => setCategory(e.target.value)}
             >
-                {categories.map((categoryElement,index) => (
-                    <option key={index} value={categoryElement}>{categoryElement}</option>
-                ))}
-
+              {categories.map((categoryElement, index) => (
+                <option key={index} value={categoryElement}>{categoryElement}</option>
+              ))}
             </select>
             <div className="error-message">{categoryError}</div>
           </div>
@@ -180,8 +186,8 @@ function InventoryEditForm() {
                 value="In Stock"
                 id="inStock"
                 onChange={handleStatusChange}
-                checked={status === 'In Stock' && true}
-              /> 
+                checked={status === 'In Stock'}
+              />
               <label className="radio-instock">
                 In Stock
               </label>
@@ -192,13 +198,27 @@ function InventoryEditForm() {
                 value="Out of Stock"
                 id="outOfStock"
                 onChange={handleStatusChange}
-                checked={status === 'Out of Stock' && true}
+                checked={status === 'Out of Stock'}
               />
               <label className="radio-outofstock">
                 Out of Stock
               </label>
             </div>
           </div>
+          {status === "In Stock" && (
+            <div className="inventoryDetailsForm__field-container inventoryDetailsForm__item-container">
+              <label>Quantity</label>
+              <br />
+              <input
+                className={`inventoryDetailsForm__phone-number-input ${quantityError && "error"}`}
+                type="text"
+                name="quantity"
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+              <div className="error-message">{quantityError}</div>
+            </div>
+          )}
           <div className="inventoryDetailsForm__field-container inventoryDetailsForm__item-container">
             <label>Warehouse</label>
             <br />
@@ -208,7 +228,9 @@ function InventoryEditForm() {
               value={warehouse}
               onChange={(e) => setWarehouse(e.target.value)}
             >
-                {warehouseList.map(warehouse=> <option key={  warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>)}
+              {warehouseList.map(warehouse => (
+                <option key={warehouse.id} value={warehouse.id}>{warehouse.warehouse_name}</option>
+              ))}
             </select>
             <div className="error-message">{warehouseError}</div>
           </div>
